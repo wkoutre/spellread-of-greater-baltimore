5588a04197e2c2ec3992f0c21c9d99432a83643f
Nick Koutrelakos <nick.koutrelakos@gmail.com>
2017-04-08 15:47:21 -0700

    Default addition

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,49 +15,51 @@ class Contact extends React.Component {
 		          transitionEnterTimeout={500}
 		          transitionLeaveTimeout={500}
 		          className="page-wrapper contact-wrapper">
-				<h1>Contact Us</h1>
-				<div className="contact-text-pic">
-					{text}
-				<figure>
-					<img src={Joan} alt="Director Joan Collins and her granddaughter, Katie"/>
-					<figcaption>Joan Collins, Director</figcaption>	
-				</figure>	
-				
+		          <div className="contact">
+					<h1>Contact Us</h1>
+					<div className="contact-text-pic">
+						{text}
+					<figure>
+						<img src={Joan} alt="Director Joan Collins and her granddaughter, Katie"/>
+						<figcaption>Joan Collins, Director</figcaption>	
+					</figure>	
+					
+					</div>
+					<form
+						className="contact-form"
+						autoComplete="on"
+						action="">
+						<div><span>*First Name:</span> <input
+							required
+							type="text"
+							name="firstname"
+							autoComplete="on"
+							placeholder="*First name"/></div>
+						<div><span>*Last Name:</span> <input required
+							type="text" 
+							name="lastname"
+							autoComplete="on"
+							placeholder="*Last name"/></div>
+						<div><span>*Email: </span><input 
+							required 
+							type="email" 
+							name="user_email" 
+							autoComplete="on" 
+							placeholder="*Email address"/></div>
+						<div className="comment-box"><span>*Comments: </span><textarea 
+							required 
+							name="" 
+							id="" 
+							cols="30" 
+							rows="10" 
+							maxLength="500"
+							placeholder="*Enter your comments here"></textarea></div>
+						<button
+							type="submit">
+							<span>Submit</span>
+						</button>
+					</form>
 				</div>
-				<form
-					className="contact-form"
-					autoComplete="on"
-					action="">
-					<div><span>*First Name:</span> <input
-						required
-						type="text"
-						name="firstname"
-						autoComplete="on"
-						placeholder="*First name"/></div>
-					<div><span>*Last Name:</span> <input required
-						type="text" 
-						name="lastname"
-						autoComplete="on"
-						placeholder="*Last name"/></div>
-					<div><span>*Email: </span><input 
-						required 
-						type="email" 
-						name="user_email" 
-						autoComplete="on" 
-						placeholder="*Email address"/></div>
-					<div className="comment-box"><span>*Comments: </span><textarea 
-						required 
-						name="" 
-						id="" 
-						cols="30" 
-						rows="10" 
-						maxLength="500"
-						placeholder="*Enter your comments here"></textarea></div>
-					<button
-						type="submit">
-						<span>Submit</span>
-					</button>
-				</form>
 			</CSSTransitionGroup>
 		)
 	}
